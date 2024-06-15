8c173757d7930c5a5cd8f76def1ce426675d63a2
alex_feldshteyn <alexey.israel22@gmail.com>
2024-06-15 20:09:38 +0300

    added bar chart on client

--- client/src/store/slices.ts
+++ client/src/store/slices.ts
@@ -1,14 +1,22 @@
 import { createSlice } from '@reduxjs/toolkit'
 import type { PayloadAction } from '@reduxjs/toolkit'
-import { TransactionsDataType } from '../types/transaction-type'
+import { TransactionsPerPeriod, TransactionsSummaryPerPeriod } from '../types/transaction-type'
 
 interface TransactionsState {
-  data: TransactionsDataType []
+  data: TransactionsPerPeriod []
+  summaryPerPeriod: TransactionsSummaryPerPeriod
   isDark: boolean
 }
 
 const initialState: TransactionsState = {
   data: [],
+  summaryPerPeriod: {
+    userId: '',
+    periodTitle: '',
+    incomes: [],
+    outcomes: [],
+    labels: []
+  },
   isDark: false
 }
 
@@ -17,16 +25,18 @@ export const transactionsSlice = createSlice({
   // `createSlice` will infer the state type from the `initialState` argument
   initialState,
   reducers: {
-    setTransactionsData: (state: TransactionsState, action: PayloadAction<TransactionsDataType[]>) => {
+    setTransactionsData: (state: TransactionsState, action: PayloadAction<TransactionsPerPeriod[]>) => {
         state.data = action.payload;
     },
+    setTransactionsSummaryPerPeriod: (state: TransactionsState, action: PayloadAction<TransactionsSummaryPerPeriod>) => {
+      state.summaryPerPeriod = action.payload;
+  },
     setTheme: (state: TransactionsState) => { 
         state.isDark = !state.isDark;
     }
-
   },
 })
 
-export const { setTransactionsData, setTheme } = transactionsSlice.actions
+export const { setTransactionsData, setTransactionsSummaryPerPeriod, setTheme } = transactionsSlice.actions
 
 export default transactionsSlice.reducer
